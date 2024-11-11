PixelExperience/frameworks_base

Implement DeviceConfigUtils and service
- Allow overriding device configs while using GMS as device configurator package

Change-Id: Ie7327610feca7a390915511088dfef831c946853

services/core/java/com/android/server/custom/CustomDeviceConfigService.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2023 The PixelExperience Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.custom;
+
+import android.content.Context;
+import com.android.server.SystemService;
+
+import com.android.internal.util.custom.DeviceConfigUtils;
+
+public class CustomDeviceConfigService extends SystemService {
+
+    private static final String TAG = "CustomDeviceConfigService";
+
+    private final Context mContext;
+
+    public CustomDeviceConfigService(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            DeviceConfigUtils.setDefaultProperties(mContext.getContentResolver());
+        }
+    }
+}

services/java/com/android/server/SystemServer.java
@@ -208,6 +208,7 @@ import com.android.server.twilight.TwilightService;
 import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.UsageStatsService;
 import com.android.server.utils.TimingsTraceAndSlog;
+import com.android.server.custom.CustomDeviceConfigService;
 import com.android.server.vibrator.LinearmotorVibratorService;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.vr.VrManagerService;
@@ -2521,6 +2522,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
+            t.traceEnd();
+
+            // CustomDeviceConfigService
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
             t.traceEnd();
 
             t.traceBegin("StartLinearmotorVibratorService");

drkphnx/android_frameworks_base

DeviceConfigUtils: Make debug optional
Change-Id: I65ffac8fd0a9971297b8d721d860face571a2d44
(cherry picked from commit 9f54af5392d7ca70e1e495fc97560538459844e7)
DeviceConfig: Change setProperties behavior
Change-Id: I7cf1574f423c7362e6a7c9d8a002e4b20e5e7f87
(cherry picked from commit f94a2c97e7bf53ee6681183f2394878369e99470)
* Adapt to Android 14
Change-Id: I6ece899c13f12ee299ba24b02f2b4ab96f3d92fb
Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/crdroid/CustomDeviceConfigService.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2023 The PixelExperience Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.server.crdroid;
+
+import android.content.Context;
+import com.android.server.SystemService;
+
+import com.android.internal.util.crdroid.DeviceConfigUtils;
+
+public class CustomDeviceConfigService extends SystemService {
+
+    private static final String TAG = "CustomDeviceConfigService";
+
+    private final Context mContext;
+
+    public CustomDeviceConfigService(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            DeviceConfigUtils.setDefaultProperties(null, null);
+        }
+    }
+}

services/java/com/android/server/SystemServer.java
@@ -153,6 +153,7 @@ import com.android.server.contentsuggestions.ContentSuggestionsManagerService;
 import com.android.server.contextualsearch.ContextualSearchManagerService;
 import com.android.server.coverage.CoverageService;
 import com.android.server.cpu.CpuMonitorService;
+import com.android.server.crdroid.CustomDeviceConfigService;
 import com.android.server.credentials.CredentialManagerService;
 import com.android.server.criticalevents.CriticalEventLog;
 import com.android.server.devicepolicy.DevicePolicyManagerService;
@@ -2669,6 +2670,11 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartBackgroundInstallControlService");
             mSystemServiceManager.startService(BackgroundInstallControlService.class);
+            t.traceEnd();
+
+            // CustomDeviceConfigService
+            t.traceBegin("StartCustomDeviceConfigService");
+            mSystemServiceManager.startService(CustomDeviceConfigService.class);
             t.traceEnd();
 
             if (context.getResources().getBoolean(