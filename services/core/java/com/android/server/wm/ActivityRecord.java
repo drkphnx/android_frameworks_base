7e473df270e0f7934306ea84af4d8fc10367f239
priiii08918 <priyanshusinghal0818@proton.me>
2025-01-15 10:37:01 +0000

    [SQUASH] services: Drop boost optimizations
    Revert "services: Implement exit app animation boost"
    
    This reverts commit 3b82cf16bf4f78816b4cba316830ecb9ff2c5e11.
    
    Revert "services: Implement activity boost optimization"
    
    This reverts commit f0d5e032a52a87a4258b62104dfddbaeb5c2a64c.
    
    Revert "services: Implement transition boosting"
    
    This reverts commit ab4a1275df855e006f93f1a406677c8cc3338c0d.
    
    Revert "services: Implement scroll gesture boosting"
    
    This reverts commit 7bc12a0badcaebce75f91ef5f0413683ebb2d0e4.

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -306,7 +306,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.gui.DropInputMode;
 import android.hardware.HardwareBuffer;
-import android.hardware.power.Mode;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
@@ -315,7 +314,6 @@ import android.os.Debug;
 import android.os.IBinder;
 import android.os.IRemoteCallback;
 import android.os.PersistableBundle;
-import android.os.PowerManagerInternal;
 import android.os.Process;
 import android.os.RemoteCallbackList;
 import android.os.RemoteException;
@@ -970,9 +968,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
     /** Non-zero to pause dispatching configuration changes to the client. */
     int mPauseConfigurationDispatchCount = 0;
-
-    private final PowerManagerInternal mPowerManagerInternal;
-    private boolean mIsBoosted;
 
     private final Runnable mPauseTimeoutRunnable = new Runnable() {
         @Override
@@ -2182,8 +2177,6 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                             return appContext;
                         });
         mCallerState = new ActivityCallerState(mAtmService);
-
-        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     private boolean isAppActivityEmbeddingSplitsEnabled() {
@@ -6277,13 +6270,12 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
                 Slog.v(TAG_VISIBILITY, "Start visible activity, " + this);
             }
             setState(STARTED, "makeActiveIfNeeded");
-            setActivityBoost(true);
+
             try {
                 mAtmService.getLifecycleManager().scheduleTransactionItem(app.getThread(),
                         new StartActivityItem(token, takeSceneTransitionInfo()));
             } catch (Exception e) {
                 Slog.w(TAG, "Exception thrown sending start: " + intent.getComponent(), e);
-                setActivityBoost(false);
             }
             // The activity may be waiting for stop, but that is no longer appropriate if we are
             // starting the activity again
@@ -6909,16 +6901,8 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         }
     }
 
-    protected void setActivityBoost(boolean enable) {
-        if (mIsBoosted != enable && mPowerManagerInternal != null) {
-            mIsBoosted = enable;
-            mPowerManagerInternal.setPowerMode(Mode.LAUNCH, enable);
-        }
-    }
-
     /** Called when the windows associated app window container are drawn. */
     private void onWindowsDrawn() {
-        setActivityBoost(false);
         final TransitionInfoSnapshot info = mTaskSupervisor
                 .getActivityMetricsLogger().notifyWindowsDrawn(this);
         final boolean validInfo = info != null;
--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -92,9 +92,7 @@ import android.content.res.Configuration;
 import android.graphics.Insets;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.hardware.power.Boost;
 import android.os.IBinder;
-import android.os.PowerManagerInternal;
 import android.os.UserHandle;
 import android.util.DisplayMetrics;
 import android.util.Slog;
@@ -110,7 +108,6 @@ import android.window.TaskFragmentOrganizerToken;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.protolog.ProtoLog;
 import com.android.internal.util.ToBooleanFunction;
-import com.android.server.LocalServices;
 import com.android.server.am.HostingRecord;
 import com.android.server.pm.pkg.AndroidPackage;
 import com.android.window.flags.Flags;
@@ -416,8 +413,6 @@ class TaskFragment extends WindowContainer<WindowContainer> {
     private final EnsureActivitiesVisibleHelper mEnsureActivitiesVisibleHelper =
             new EnsureActivitiesVisibleHelper(this);
 
-     private final PowerManagerInternal mPowerManagerInternal;
-
     /** Creates an embedded task fragment. */
     TaskFragment(ActivityTaskManagerService atmService, IBinder fragmentToken,
             boolean createdByOrganizer) {
@@ -438,7 +433,6 @@ class TaskFragment extends WindowContainer<WindowContainer> {
                 mAtmService.mWindowOrganizerController.mTaskFragmentOrganizerController;
         mFragmentToken = fragmentToken;
         mRemoteToken = new RemoteToken(this);
-        mPowerManagerInternal = LocalServices.getService(PowerManagerInternal.class);
     }
 
     @NonNull
@@ -1544,9 +1538,6 @@ class TaskFragment extends WindowContainer<WindowContainer> {
                 dc.prepareAppTransition(TRANSIT_NONE);
             } else {
                 dc.prepareAppTransition(TRANSIT_OPEN);
-                if (mPowerManagerInternal != null) {
-                    mPowerManagerInternal.setPowerBoost(Boost.DISPLAY_UPDATE_IMMINENT, 80);
-                }
             }
         }
 
