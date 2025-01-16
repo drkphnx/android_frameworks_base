fdc6b03d55cba743a719e73333c3df02908d33fa
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-16 15:10:46 +0800

    Hide notification scrim when QS transparency is low
    reference issue: https://github.com/RisingTechOSS/issue_tracker/issues/80

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1001,6 +1001,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             state.setCustomScrimAlpha(mCustomScrimAlpha);
         }
         applyState();
+        updateNotificationScrimVisibility();
     }
 
     private void applyState() {
@@ -1792,9 +1793,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     
     private void updateNotificationScrimVisibility() {
         if (mNotificationsScrim == null) return;
+        boolean hideNotifScrim = (mCustomScrimAlpha <= 0.4f 
+            || (com.android.systemui.qs.TileUtils.canShowSplitShade(mScrimBehind.getContext()) 
+            && mIsLandscape));
         mNotificationsScrim.setVisibility(
-            com.android.systemui.qs.TileUtils.canShowSplitShade(mScrimBehind.getContext()) 
-            && mIsLandscape 
+            hideNotifScrim
             ? View.GONE : View.VISIBLE);
     }
 
